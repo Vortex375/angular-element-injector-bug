6638b6cec755cddaf0f8938f5550f7a8ea17b955
Benjamin Schmitz <vortex@wolpzone.de>
2019-05-16 13:07:59 +0200

    create runnable example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, InjectionToken } from '@angular/core';
+
+export const EXAMPLE_TOKEN = new InjectionToken<string>('app.exampleToken');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [{ provide: EXAMPLE_TOKEN, useValue: 'Value from AppComponent' }]
 })
-export class AppComponent {
-  title = 'test-app';
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Example1Directive } from './example1.directive';
+import { Example2Directive } from './example2.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Example1Directive,
+    Example2Directive
   ],
   imports: [
     BrowserModule
